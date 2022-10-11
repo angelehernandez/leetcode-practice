3d943f49c71b35ad8dac8aeeb63cf0dd95465c1c
Angel Hernandez <angelhernandez2021@u.northwestern.edu>
2022-10-11 18:37:38 -0500

    problem 5 setup

--- problem5.py
+++ problem5.py
@@ -1,9 +1,19 @@
 class Solution:
+    '''
+    Given a string s, return the longest palindromic substring in s.
+    A string is called a palindrome string if the reverse of that string is the same as the original string.
+    '''
+    inputs = ["babad", "bab"]
+
+    def __init__(self):
+        for s in self.inputs:
+            print(self.longestPalindrome(s))
+
     def longestPalindrome(self, s: str) -> str:
         pass
 
 def main():
-    pass
+    Solution()
 
 if __name__=="__main__":
     main()
