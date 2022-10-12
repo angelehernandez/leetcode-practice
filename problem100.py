5b692994fde68a61a28b7c2b378146f59d2f6818
Angel Hernandez <angelhernandez2021@u.northwestern.edu>
2022-10-12 17:36:09 -0500

    add completed problem 100 BFS method

--- problem100.py
+++ problem100.py
@@ -1,16 +1,87 @@
 # Definition for a binary tree node.
 class TreeNode:
+
+    # this is not the true class definition
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
 
 class Solution:
-    '''Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+    """Given the roots of two binary trees p and q, write a function to check if they are the same or not.
     Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
-    '''
+    """
+
+    inputs = [
+        ([1,2,3], [1,2,3]),
+        ([1,2], [1,None,2]),
+        ([1,2,1], [1,1,2]),
+        ([None, None, None], [None, None, None]),
+    ]
+    outputs = [
+        True,
+        False,
+        False,
+        True,
+        ]
+
+    def __init__(self):
+        results = []
+        for i in range(len(self.inputs)):
+            results.append(self.isSameTree(
+                TreeNode(self.inputs[i][0]), #p
+                TreeNode(self.inputs[i][1]), #q
+            ))
+            print(f"Test[{i}] Result: {results[i]}")
+        if results == self.outputs:
+            print("All tests passed.")
+        else:
+            print("At least one test failed.")
+
+    def BFS_method(self, p, q) -> bool:
+        """Verifies that trees are equal using BFS method
+        
+        Args:
+            p: tree with [0,100] nodes
+            q: tree with [0,100] nodes
+            
+        Returns:
+            boolean of whether or not trees are equal    
+        """
+        pqueue = [p]
+        qqueue = [q]
+
+
+        # one or both trees are empty
+        if (p is None) and (q is None):
+            return True
+        elif (p is None) or (q is None):
+            return False
+
+        while (len(pqueue) > 0) and (len(qqueue) > 0):
+            # extract node
+            u = pqueue.pop(0)
+            v = qqueue.pop(0)
+            
+            # check if equal
+            if u is None and v is None:
+                continue
+            elif u is None or v is None:
+                return False
+            if u.val != v.val:
+                return False
+
+            # add next nodes if they exist
+            pqueue.append(u.left)
+            pqueue.append(u.right)
+            qqueue.append(v.left)
+            qqueue.append(v.right)
+        
+        # all nodes verified
+        return True
+
     def isSameTree(self, p, q) -> bool:
-        pass
+        return self.BFS_method(p, q) 
 
 def main():
     Solution()
